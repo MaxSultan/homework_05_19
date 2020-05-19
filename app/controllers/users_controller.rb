f3e469aa513dee206f55e758389b6516d25807df
Max Sultan <maxsultakent@gmail.com>
2020-05-19 16:32:14 -0600

    finished all CRUD actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,17 +23,21 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end 
 
-    if User.update(users_params)
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(users_params)
       redirect_to users_path
     else 
       render :edit
     end 
   end
-
+ 
   def destroy
-    @user = User.find(params[:id]).destroy
-    redirect_to users_index_path
+    User.find(params[:id]).destroy
+    redirect_to users_path
   end 
 
   private
