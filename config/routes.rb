c473b4edd92ae09a98271f1c54ae5d3007e00ecf
Max Sultan <maxsultakent@gmail.com>
2020-05-19 15:59:04 -0600

    new and show methods working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  root 'users#index'
+
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
 end
